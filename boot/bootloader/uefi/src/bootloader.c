02d7996b62851d7cea5da70683995fda51ce91ff
wizrd <wizrd.bin@proton.me>
2026-09-08 11:35:00 -0400

    Obiously, I don't need complicated algorithm to just map kernel

--- boot/bootloader/uefi/src/bootloader.c
+++ boot/bootloader/uefi/src/bootloader.c
@@ -225,63 +225,12 @@ efi_map_page(size_t real_addr, size_t virt_addr)
 	real_addr = real_addr & 0xffffffffffff;
 	virt_addr = virt_addr & 0xffffffffffff;
 
-	struct index *last_pml4i;
 	unsigned int pml4_index = (unsigned int)(virt_addr >> 39);
-	efi_printf("pml4_index : %d \r\n", pml4_index);
-	if (!search_pml4i(pml4_index, &last_pml4i)) {
-		EFI_PHYSICAL_ADDRESS Memory;
-		status = SysTab->BootServices->AllocatePages(AllocateAnyPages,
-		    EfiLoaderData, (UINT64) 1, &Memory);
-		CHECK_STATUS(status, MAP_ERROR_ALLOCATE_PAGE);
-		efi_memset((void *)Memory, 0, (size_t)PAGE_SIZE);
-		status = SysTab->BootServices->AllocatePool(EfiLoaderData,
-		    sizeof(struct index), (VOID **) &last_pml4i->next);
-		CHECK_STATUS(status, MAP_ERROR_ALLOCATE_POOL);
-		pml4[pml4_index] = Memory | PML4_ENTRY_FLAGS;
-		last_pml4i->next->value = (UINT16)pml4_index;
-		last_pml4i->next->next = NULL;
-	}
-	UINT64 *pdpt = (UINT64 *)(pml4[pml4_index] & 0xfffffffffffff000);
-
-	struct index *last_pdpti;
 	unsigned int pdpt_index = (unsigned int)((virt_addr >> 30) & 0x1ff);
-	efi_printf("pdpt_index : %d \r\n", pdpt_index);
-	if (!search_pdpti(pdpt_index, &last_pdpti)) {
-		EFI_PHYSICAL_ADDRESS Memory;
-		status = SysTab->BootServices->AllocatePages(AllocateAnyPages,
-		    EfiLoaderData, (UINT64) 1, &Memory);
-		CHECK_STATUS(status, MAP_ERROR_ALLOCATE_PAGE);
-		efi_memset((void *)Memory, 0, (size_t)PAGE_SIZE);
-		status = SysTab->BootServices->AllocatePool(EfiLoaderData,
-		    sizeof(struct index), (VOID **) &last_pdpti->next);
-		CHECK_STATUS(status, MAP_ERROR_ALLOCATE_POOL);
-		pdpt[pdpt_index] = Memory | PDPT_ENTRY_FLAGS;
-		last_pdpti->next->value = (UINT16)pdpt_index;
-		last_pdpti->next->next = NULL;
-	}
-	UINT64 *pd = (UINT64 *)(pdpt[pdpt_index] & 0xfffffffffffff000);
-
-	struct index *last_pdi;
 	unsigned int pd_index = (unsigned int)((virt_addr >> 21) & 0x1ff);
-	efi_printf("pd_index : %d \r\n", pd_index);
-	if (!search_pdi(pd_index, &last_pdi)) {
-		EFI_PHYSICAL_ADDRESS Memory;
-		status = SysTab->BootServices->AllocatePages(AllocateAnyPages,
-		    EfiLoaderData, (UINT64) 1, &Memory);
-		CHECK_STATUS(status, MAP_ERROR_ALLOCATE_PAGE);
-		efi_memset((void *)Memory, 0, (size_t)PAGE_SIZE);
-		status = SysTab->BootServices->AllocatePool(EfiLoaderData,
-		    sizeof(struct index), (VOID **) &last_pdi->next);
-		CHECK_STATUS(status, MAP_ERROR_ALLOCATE_POOL);
-		pd[pd_index] = Memory | PD_ENTRY_FLAGS;
-		last_pdi->next->value = (UINT16)pd_index;
-		last_pdi->next->next = NULL;
-	}
-	UINT64 *pt = (UINT64 *)(pd[pd_index] & 0xfffffffffffff000);
-
 	unsigned int pt_index = (unsigned int)((virt_addr >> 12) & 0x1ff);
-	efi_printf("pt_index : %d \r\n", pt_index);
-	pt[pt_index] = (UINT64)real_addr | PT_ENTRY_FLAGS;
+
+	/* TODO */
 
 	return ret;
 }
