87df7c67e43e2a4a05eeda1d5f4db8e9dbb76974
wizrd <wizrd.bin@proton.me>
2026-09-16 10:36:47 -0400

    paging the physical memory is almost done

--- kern/src/venux.c
+++ kern/src/venux.c
@@ -1,27 +1,83 @@
 #include "venux.h"
 
 uint64_t *phys_pml4 = NULL;
+uint64_t pml4e_flags = 1;
+uint64_t pdpte_flags = 1;
+uint64_t pde_flags = 1;
+uint64_t pte_flags = 1;
 
-static uint64_t
-entry_flags(uint64_t attr)
+static void
+set_entry_flags(uint32_t type, uint64_t attr)
 {
-	return attr;
+	return;
 }
 
 static int
-kern_map_physmem_pdpt(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e,
-    uint64_t attr, uint64_t *pdpt)
+kern_map_physmem_pt(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e,
+    uint64_t *pt)
 {
 	int ret = 0;
 	while (vaddr_s < vaddr_e) {
-		
+		int pti = (int)GET_PTI(vaddr_s);
+		if (ENTRY_PRESENT(pt[pti]))
+			return ret = KERN_ERROR_PAGE_PRESENT;
+		pt[pti] = paddr_s | pte_flags;
+		paddr_s += PAGE_SIZE;
+		vaddr_s += PAGE_SIZE;
 	}
 	return ret;
 }
 
 static int
-kern_map_physmem_pml4(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e,
-    uint64_t attr)
+kern_map_physmem_pd(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e,
+    uint64_t *pd)
+{
+	int ret = 0;
+	while (vaddr_s < vaddr_e) {
+		int pdi = (int)GET_PDI(vaddr_s);
+		uint64_t bound_e = ((vaddr_s | 0x1fffff) > vaddr_e) ?
+		    vaddr_e : vaddr_s | 0x1fffff;
+		if (!ENTRY_PRESENT(pd[pdi])) {
+			ALLOC_PT(pd[pdi], ret, pde_flags);
+			if (RET_ERROR(ret))
+				return ret;
+		}
+		uint64_t *pt = (uint64_t *)(pd[pdi] & 0xfffffffffffff000);
+		ret = kern_map_physmem_pt(paddr_s, vaddr_s, vaddr_e, pt);
+		if (RET_ERROR(ret))
+			return ret;
+		paddr_s += bound_e - vaddr_s;
+		vaddr_s += bound_e - vaddr_s;
+	}
+	return ret;
+}
+
+static int
+kern_map_physmem_pdpt(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e,
+    uint64_t *pdpt)
+{
+	int ret = 0;
+	while (vaddr_s < vaddr_e) {
+		int pdpti = (int)GET_PDPTI(vaddr_s);
+		uint64_t bound_e = ((vaddr_s | 0x3fffffff) > vaddr_e) ?
+		    vaddr_e : vaddr_s | 0x3fffffff;
+		if (!ENTRY_PRESENT(pdpt[pdpti])) {
+			ALLOC_PD(pdpt[pdpti], ret, pdpte_flags);
+			if (RET_ERROR(ret))
+				return ret;
+		}
+		uint64_t *pd = (uint64_t *)(pdpt[pdpti] & 0xfffffffffffff000);
+		ret = kern_map_physmem_pd(paddr_s, vaddr_s, vaddr_e, pd);
+		if (RET_ERROR(ret))
+			return ret;
+		paddr_s += bound_e - vaddr_s;
+		vaddr_s += bound_e - vaddr_s;
+	}
+	return ret;
+}
+
+static int
+kern_map_physmem_pml4(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e)
 {
 	int ret = 0;
 	while (vaddr_s < vaddr_e) {
@@ -29,13 +85,13 @@ kern_map_physmem_pml4(uint64_t paddr_s, uint64_t vaddr_s, uint64_t vaddr_e,
 		uint64_t bound_e = ((vaddr_s | 0x7fffffffff) > vaddr_e) ?
 		    vaddr_e : vaddr_s | 0x7fffffffff;
 		if (!ENTRY_PRESENT(phys_pml4[pml4i])) {
-			ALLOC_PDPT(phys_pml4[pml4i], ret, entry_flags(attr));
+			ALLOC_PDPT(phys_pml4[pml4i], ret, pml4e_flags);
 			if (RET_ERROR(ret))
 				return ret;
 		}
-		uint64_t *pdpt = phys_pml4[pml4i] & 0xfffffffffffff000;
-		ret = kern_map_physmem_pdpt(paddr_s, vaddr_s, vaddr_e, attr,
-		    pdpt);
+		uint64_t *pdpt = (uint64_t *)(phys_pml4[pml4i] &
+		    0xfffffffffffff000);
+		ret = kern_map_physmem_pdpt(paddr_s, vaddr_s, vaddr_e, pdpt);
 		if (RET_ERROR(ret))
 			return ret;
 		paddr_s += bound_e - vaddr_s;
@@ -48,6 +104,7 @@ static int
 kern_map_physmem_desc(struct mem_desc *desc)
 {
 	int ret = 0;
+	set_entry_flags(desc->type, desc->attr);
 	if (phys_pml4 == NULL) {
 		ALLOC_PML4(phys_pml4, ret);
 		if (RET_ERROR(ret))
@@ -55,8 +112,7 @@ kern_map_physmem_desc(struct mem_desc *desc)
 	}
 	return kern_map_physmem_pml4((uint64_t)desc->phys_start,
 	    (uint64_t)desc->virt_start,
-	    (uint64_t)desc->virt_start + desc->page_count * PAGE_SIZE,
-	    desc->attr);
+	    (uint64_t)desc->virt_start + desc->page_count * PAGE_SIZE);
 }
 
 static int
@@ -73,6 +129,7 @@ kern_map_physmem(struct mem_info *mem)
 			return ret;
 		ptr += mem->size;
 	}
+	return ret;
 }
 
 void
